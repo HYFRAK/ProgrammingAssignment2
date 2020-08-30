0550f5a7883c3e2056f13d7b9c331c345ea43940
HYFRAK <69751274+HYFRAK@users.noreply.github.com>
2020-08-30 14:11:11 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,7 @@
-makeCacheMatrix <- function(x = matrix()){
-  m <- NULL
+## A couple of functions to calculate the inverse of a matrix passed as input.
+
+makeCacheMatrix <- function(x = matrix()){.  #initializing a function that creates a list of functions to use during execution of next function
+  m <- NULL. 
   set <- function(y){
     x <<- y
     m <<- NULL
@@ -7,12 +9,12 @@ makeCacheMatrix <- function(x = matrix()){
   get <- function() x
   setinverse <- function(inverse) m <<- inverse
   getinverse <- function() m
-  list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
+  list(set = set, get = get, setinverse = setinverse, getinverse = getinverse) #this function returns the list of functions with the matrix variable to calculate the inverse
 }
 
-cacheSolve <- function(x, ...){
+cacheSolve <- function(x, ...){ #initializing a function that uses a list of functions created during execution of previous function to calculate inverse of the matrix
   m <- x$getinverse()
-  if(!is.null(m)) {
+  if(!is.null(m)) {    #if m already has cached values the function returns m, else it continues calculation
     message("getting cached data")
     return(m)
   }
@@ -20,6 +22,11 @@ cacheSolve <- function(x, ...){
   m <- solve(data, ...)
   x$setinverse(m)
   m
-}
+} # inverse of the matrix is calculated and returned
+
+## USAGE
+mat1 <- matrix(rnorm(9), nrows = 3, ncols = 3) #initialize any matrtix
+cacheSolve(makeCacheMatrix(mat1)) #pass the matrix through the function to calculate the inverse
+
 
 
